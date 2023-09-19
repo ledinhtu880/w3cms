ab3745e9c0822de34b55ed661300050e8200fe6c
Sweety <ledinhtu880@gmail.com>
2023-09-19 15:01:37 +0700

    C2: Edit file index.php

--- index.php
+++ index.php
@@ -16,8 +16,8 @@
   <link rel="stylesheet" href="./assets/css/base.css">
   <link rel="stylesheet" href="./assets/css/style.css">
   <!-- Vendor CSS Files -->
-  <link rel="stylesheet" href="./assets/icons-1.11.0/font/bootstrap-icons.css">
-  <link rel="stylesheet" href="./assets/fontawesome-free-6.4.0-web/css/all.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 
 </head>
 
